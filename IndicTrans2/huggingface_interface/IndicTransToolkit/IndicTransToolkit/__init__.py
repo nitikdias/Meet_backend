3b58c39048c4790dbd3556a8ef937ea781f5102f
nitikdias <nitik@arcaai.com>
2025-05-15 11:53:24 +0530

    Delete IndicTrans2/huggingface_interface directory

--- IndicTrans2/huggingface_interface/IndicTransToolkit/IndicTransToolkit/__init__.py
+++
@@ -1,9 +0,0 @@
-from .evaluator import IndicEvaluator
-from .collator import IndicDataCollator
-from .processor import IndicProcessor
-
-__all__ = [
-    "IndicEvaluator",
-    "IndicDataCollator",
-    "IndicProcessor",
-]
